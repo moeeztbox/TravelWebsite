a3b05d3b81b4cceac2d21d2c9bf245d5d4101ca8
moeeztbox <moeez.tbox@gmail.com>
2025-07-24 13:31:09 +0500

    Just add footer,copyright and navbar file.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,20 +6,28 @@ import Booking from "./Pages/Booking";
 import Packages from "./Pages/Packages";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
+import Copyright from "./Components/Main/CopyRight";
+import Navbar from "./Components/Main/Navbar";
+import Footer from "./Components/Main/Footer";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/packages" element={<Packages />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/packages" element={<Packages />} />
+        </Routes>
+      </Router>
+      <Footer />
+      <Copyright />
+    </>
   );
 }
 
---
+++ Frontend/src/Components/Main/CopyRight.jsx
@@ -0,0 +1,11 @@
+function Copyright() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="text-center py-4 text-gray-500 bg-black text-sm">
+      &copy; {year} Your Company Name. All rights reserved.
+    </footer>
+  );
+}
+
+export default Copyright;
