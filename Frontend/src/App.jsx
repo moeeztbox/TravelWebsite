4ed10852ed3b307334248312284290daa36ae17c
Benji <acread194@gmail.com>
2025-08-11 04:11:08 +0500

    Added FAQ Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
@@ -11,13 +13,32 @@ import Copyright from "./Components/Main/CopyRight";
 // import Navbar from "./Components/Main/Navbar - Copy";
 import Navbar from "./Components/Main/ReNavbar";
 import Footer from "./Components/Main/Footer";
+import FAQ from "./Pages/FAQ";
+
+// ScrollToTop component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to top when route changes
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth' // Use 'auto' for instant scroll
+    });
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/faq" element={<FAQ />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/login" element={<Login />} />
